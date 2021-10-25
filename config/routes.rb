889950cd1d107d78c354804ba8f5fee03b86f64a
britneykline <87348224+britneykline@users.noreply.github.com>
2021-10-25 18:59:14 -0400

    created all routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  get 'rides/index'
-  get 'rides/new'
-  get 'rides/create'
-  get 'rides/destroy'
-  get 'passengers/index'
-  get 'passengers/new'
-  get 'passengers/create'
-  get 'passengers/show'
-  get 'passengers/edit'
-  get 'passengers/update'
-  get 'passengers/destroy'
+  get 'rides' to:'rides#index'
+  get 'rides/new' to:'rides#new'
+  post 'rides' to:'rides#create'
+  delete 'rides/:id' to: 'rides#destroy'
+  get 'passengers', to: 'passengers#index'
+  get 'passengers/new', to:'passengers#new', as: :new_passenger
+  post 'passengers', to:'passengers/create'
+  get 'passengers/:id', to:'passengers/show'
+  get 'passengers/:id/edit', to:'passengers/edit'
+  patch 'passengers/:id', to: 'passengers/update'
+  delete 'passengers/:id', to:'passengers#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
