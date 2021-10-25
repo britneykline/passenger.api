5aa6f3169099ca23ded9e6c11104146b0cb788dc
britneykline <87348224+britneykline@users.noreply.github.com>
2021-10-25 18:46:47 -0400

    create all controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'passengers/index'
+  get 'passengers/new'
+  get 'passengers/create'
+  get 'passengers/show'
+  get 'passengers/edit'
+  get 'passengers/update'
+  get 'passengers/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
